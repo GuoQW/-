0283ea3eecc8e672c509dc02e9e6a0f67de8e532
GuoQW <go@godemacbook-pro.local>
2016-07-28 19:06:19 +0800

    添加了ByeViewController控制器

---
+++ hello/hello/ByeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ByeViewController.h
+//  hello
+//
+//  Created by Go on 16/7/28.
+//  Copyright © 2016年 guoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ByeViewController : UIViewController
+
+@end
