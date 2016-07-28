652a6f739193f3bf48156703a0bfa1db8ae12a52
GuoQW <go@GodeMacBook-Pro.local>
2016-07-28 20:01:49 +0800

    添加了Guo控制器

---
+++ hello/hello/GuoViewController.h
@@ -0,0 +1,13 @@
+//
+//  GuoViewController.h
+//  hello
+//
+//  Created by Go on 16/7/28.
+//  Copyright © 2016年 guoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuoViewController : UIViewController
+
+@end
